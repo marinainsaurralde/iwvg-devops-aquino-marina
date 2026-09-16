6babf05984761fdc59af02d922b89e6d52ea13f7
Marina Aquino <marinainsaurralde11@gmail.com>
2026-09-16 13:08:41 -0300

    Updated system endpoint mapping. #3

--- src/test/java/es/upm/miw/devops/functionaltests/SystemResourceFT.java
+++ src/test/java/es/upm/miw/devops/functionaltests/SystemResourceFT.java
@@ -27,7 +27,7 @@ class SystemResourceFT {
     @Test
     void testReadBadge() {
         webTestClient.get()
-                .uri(SystemResource.VERSION_BADGE)
+                .uri(SystemResource.SYSTEM + SystemResource.VERSION_BADGE)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(String.class)
@@ -39,7 +39,7 @@ class SystemResourceFT {
     @Test
     void testReadInfo() {
         webTestClient.get()
-                .uri("/")
+                .uri(SystemResource.SYSTEM)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(String.class)
